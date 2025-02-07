2f7c1e57f9cd499e73f539f819f3e5c5f6647d07
GaveshaniSam <gaveshaniupekshanikethani@gmail.com>
2025-02-07 21:06:47 +0530

    Initial commit3

---
+++ src/chapter9/Employee.java
@@ -0,0 +1,28 @@
+package chapter9;
+
+public class Employee extends Person{
+
+    private String employeeId;
+    private String title;
+
+    public Employee(){
+        super("Gaveshani");
+        System.out.println("In Employee default constructor");
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public String getEmployeeId() {
+        return employeeId;
+    }
+
+    public void setEmployeeId(String employeeId) {
+        this.employeeId = employeeId;
+    }
+}
